e4a04eea46e41cef5049def8004fae7eeced9ff1
smhrd <smhrd@DESKTOP-R1NE47P>
2021-12-06 17:18:13 +0900

    456

--- projectColoring/target/classes/kr/color/mapper/color.sql
+++ projectColoring/target/classes/kr/color/mapper/color.sql
@@ -3,3 +3,5 @@ select * from palettes limit 5;
 select * from inputtext limit 5;
 
 select *,jaro_winkler_similarity('lavender',txt_content) as jws from inputtext order by jws desc limit 15
+
+select * from inputtext order by rand() limit 15
